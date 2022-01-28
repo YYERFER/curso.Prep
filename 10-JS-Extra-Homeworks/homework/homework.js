097f935044844b7d86d49eeab5c7d7a2f43ecb6b
YYERFER <yyerprogramador@gmail.com>
2022-01-27 19:32:12 -0500

    10-JS-EXTRA

--- 10-JS-Extra-Homeworks/homework/homework.js
+++ 10-JS-Extra-Homeworks/homework/homework.js
@@ -10,7 +10,26 @@ function deObjetoAmatriz(objeto){
       C: 3
     }) ➞ [["D", 1], ["B", 2], ["C", 3]]*/
   //Escribe tu código aquí
-}
+
+  //return Object.entries(objeto)
+    const array =[];
+   for(const clave in objeto){
+    array.push([clave,objeto[clave]])
+    
+  }
+  
+  console.log(array)
+  
+return array;
+
+}deObjetoAmatriz(
+  {
+    D: 1,
+    B: 2,
+    C: 3
+  }
+)
+
 
 
 function numberOfCharacters(string) {
@@ -18,7 +37,18 @@ function numberOfCharacters(string) {
   //en formato par clave-valor.
   //Ej: Recibe ---> "adsjfdsfsfjsdjfhacabcsbajda" || Devuelve ---> { a: 5, b: 2, c: 2, d: 4, f: 4, h:1, j: 4, s: 5 } 
   //Escribe tu código aquí
-}
+
+    let objeto ={};
+
+    for(let i=0; i<string.length; i++){
+      if(objeto.hasOwnProperty(string[i])){
+        objeto[string[i]]++
+      } else {objeto[string[i]]=1;}
+      
+    }
+    console.log(objeto);
+    return objeto
+}numberOfCharacters('aabbcccttyhsdfdfdddddd')
 
 
 function capToFront(s) {
@@ -26,6 +56,16 @@ function capToFront(s) {
   //al principio de la palabra.
   //Ejemplo: soyHENRY -> HENRYsoy
   //Escribe tu código aquí
+
+  let mayusculas =[];
+  let minusculas = [];
+
+  for(let i=0; i<s.length; i++){
+    if(s[i]===s[i].toUpperCase()){
+      mayusculas.push(s[i]);
+    }else {minusculas.push(s[i]);}
+  }
+  return mayusculas.concat(minusculas).join('')
 }
 
 
@@ -35,6 +75,15 @@ function asAmirror(str) {
   //pero con cada una de sus palabras invertidas, como si fuera un espejo.
   //Ej: Recibe ---> "The Henry Challenge is close!" || Devuelve ---> "ehT yrneH egnellahC si !esolc"
   //Escribe tu código aquí
+
+  var frase = str.split(' ').map(function(palabra){
+    return palabra.split('').reverse().join('');
+  }).join(' ');
+
+ return frase;
+
+//  function nombre(){}
+//  (str)=> str.split('').reverse().join('');
 } 
 
 
@@ -43,20 +92,59 @@ function capicua(numero){
   //La misma debe retornar: "Es capicua" si el número se número que se lee igual de 
   //izquierda a derecha que de derecha a izquierda. Caso contrario retorna "No es capicua"
   //Escribe tu código aquí
-}
+
+  var numero = numero.toString();
+  var numero2 = "";
+
+  numero2 = numero.split('').reverse().join('');
+  
+  if(numero === numero2){
+    return "Es capicua";
+  }else {return "No es capicua" }
+
+}capicua(123);
 
 
 function deleteAbc(cadena){
   //Define una función que elimine las letras "a", "b" y "c" de la cadena dada 
   //y devuelva la versión modificada o la misma cadena, en caso de contener dichas letras.
   //Escribe tu código aquí
-}
+
+  let cadena2 = "";
+
+  for(let i=0; i<cadena.length; i++){
+    if(cadena[i] !== 'a' && cadena[i]!=='b' && cadena[i]!=='c'){
+      cadena2 += cadena[i]; 
+      console.log(cadena2)
+    }
+  }
+  return cadena2;
+}deleteAbc('iogascbfdfg')
 
 
 function sortArray(arr) {
   //La función recibe una matriz de strings. Ordena la matriz en orden creciente de longitudes de cadena
   //Ej: Recibe ---> ["You", "are", "beautiful", "looking"] || Devuelve ---> [“You", "are", "looking", "beautiful"]
   //Escribe tu código aquí
+
+  // for(let i=0; i<arr.length; i++){
+  //   var temp = arr[i];
+  //   var j=j-1;
+
+  //   while(j>=0 && temp.length<arr[j].length){
+  //     arr[j+1]=arr[j]
+  //     j--
+  //   }
+  //   arr[j+1] = temp
+
+  //   console.log(arr)
+  // }
+  // return arr;
+
+  var nuevoArray = arr.sort(function(a,b){
+    return a.length - b.length;
+  })
+  return nuevoArray;
 }
 
 
@@ -66,6 +154,16 @@ function buscoInterseccion(arreglo1, arreglo2){
   //Si no tienen elementos en común, retornar un arreglo vacío.
   //Aclaración: los arreglos no necesariamente tienen la misma longitud
   //Escribe tu código aquí  
+
+  let arreglo=[];
+  for(let i=0; i<arreglo1.length; i++){
+    for(let j=0; j<arreglo2.length; j++){
+        if(arreglo1[i]===arreglo2[j]){
+          arreglo.push(arreglo1[i])
+        }
+    }
+  }
+  return arreglo;
 }
 
 
